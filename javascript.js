e55e92449e5341a1da3c9685eda73f4bcf609ce1
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-03-13 21:06:56 +0530

    Add function playGame javascript.js

--- javascript.js
+++ javascript.js
@@ -14,6 +14,8 @@ function getComputerChoice()
 }
 
 
+var winner=null;
+
 function playRound(playerSelection, computerSelection) {
     
     /*console.log(computerSelection);*/
@@ -28,33 +30,73 @@ function playRound(playerSelection, computerSelection) {
         if(playerSelection.toUpperCase()=== "ROCK" )
         {
             if(computerSelection=="Scissors")
-            str="You won!! Rock beat Scissors";
-            
+            {
+               str="You won!! Rock beat Scissors";
+               winner="player"; 
+            } 
+
             else
-            str=`You lose!! ${computerSelection} beat Rock`;
+            {
+                str=`You lose!! ${computerSelection} beat Rock`;
+                winner="computer"; 
+
+            }
         }
         else if(playerSelection.toUpperCase()=== "SCISSORS" )
         {
             if(computerSelection=="Paper")
-            str="You won!! Scissors beat Paper";
+            {
+                str="You won!! Scissors beat Paper";
+                winner="player"; 
+
+            }
             
             else
+            {
             str=`You lose!! ${computerSelection} beat Scissors`;
+            winner="computer"; 
+
+            }
         }
 
         else if(playerSelection.toUpperCase()=== "PAPER" )
         {
             if(computerSelection=="Rock")
+            {
             str="You won!! Paper beat Rock";
+            winner="player"; 
+
+            }
             
             else
+            {
             str=`You lose!! ${computerSelection} beat Paper`;
+            winner="computer"; 
+
+            }
         }
     }
     return str;
   }
   
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+
+/*const playerSelection = "rock";
 console.log(playRound(playerSelection, computerSelection));
-  
+  */
+
+function playGame()
+{
+    var player=0;
+    var computer =0;
+    for(var i=0;i<5;i++)
+    {
+        const computerSelection = getComputerChoice();
+        console.log(`computer ${computerSelection}`);
+        const playerSelection=prompt("Choose Rock, Paper or Scissors"); 
+        console.log(playRound(playerSelection, computerSelection));
+       
+    }
+
+    
+}
+playGame();
