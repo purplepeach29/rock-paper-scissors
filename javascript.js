0b5a27af8a7d1e05f638ead0b7bf1b60a356e061
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-03-13 20:07:56 +0530

    add function playRound javascript.js

--- javascript.js
+++ javascript.js
@@ -12,7 +12,49 @@ function getComputerChoice()
 
     return choice;
 }
-getComputerChoice();
 
 
+function playRound(playerSelection, computerSelection) {
+    
+    console.log(computerSelection);
+    let str=null;
+    if(playerSelection == computerSelection)
+    {
+        str="Oh no!! It's a tie";
+    }
+    else
+    {
+
+        if(playerSelection=="rock" || playerSelection=="Rock" )
+        {
+            if(computerSelection=="scissors" || computerSelection=="Scissors")
+            str="You won!! Rock beat Scissors";
+            
+            else
+            str=`You lose!! ${computerSelection} beat Rock`;
+        }
+        else if(playerSelection=="scissors" || playerSelection=="Scissor" )
+        {
+            if(computerSelection=="paper" || computerSelection=="Paper")
+            str="You won!! Scissors beat Paper";
+            
+            else
+            str=`You lose!! ${computerSelection} beat Scissors`;
+        }
+
+        else if(playerSelection=="paper" || playerSelection=="Paper" )
+        {
+            if(computerSelection=="rock" || computerSelection=="Rock")
+            str="You won!! Paper beat Rock";
+            
+            else
+            str=`You lose!! ${computerSelection} beat Paper`;
+        }
+    }
+    return str;
+  }
+  
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
   
