56b7e16ed373be8ef19cf4d85363e47a94a924d1
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-03-13 19:34:34 +0530

    add getCoputerChoice to  javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
+
 function getComputerChoice()
 {
     
+    var options=["Rock",
+    "Paper",
+    "Scissors"];
+    var index=Math.floor(Math.random() * 3);
+    var choice=options[index];
+    /*alert(choice);
+    */
+
+    return choice;
 }
+getComputerChoice();
+
+
+  
