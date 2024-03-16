d46feb7770aac56a6fd62d8b795c32ea207047b5
Prachi <63637247+purplepeach29@users.noreply.github.com>
2024-03-16 18:49:55 +0530

    Add displayComputerchoice function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,60 @@
+var player=0;
+var computer =0;
+var tieCounter=0;    
+const display=document.querySelector(".contain");
+const content=document.querySelector(".result");
+const compchoice=document.getElementById('compchoice');
+const gameOutcome = document.getElementById('gameOutcome');
+const restart = document.getElementById('restart');
+const overlay = document.getElementById('overlay');
+
+
+const btn=document.querySelectorAll("button");
+btn.forEach((button) => {
+
+    button.addEventListener('click',() => {
+
+        let computerSelection = getComputerChoice();
+        displayComputerchoice(computerSelection);
+       
+
+        content.textContent= "ROUND RESULT: " + (playRound(button.id,computerSelection)); 
+        content.append(document.createElement("BR"));
+        const newP = document.createElement('p');
+        content.append(newP);
+        if(content.textContent === "ROUND RESULT: Won"){
+        player ++;
+        newP.textContent = "YOUR SCORE: " + player +  " | " + "COMPUTER SCORE: " + computer + " | " + "TIE COUNTER: " + tieCounter;
+
+        }
+        else if(content.textContent === "ROUND RESULT: Lost"){
+            computer ++;
+            newP.textContent = "YOUR SCORE: " + player + " | " + "COMPUTER SCORE: " + computer + " | " + "TIE COUNTER: " + tieCounter;
+        }
+        else if(content.textContent === "ROUND RESULT: Tie"){
+            tieCounter ++;
+            newP.textContent = "YOUR SCORE: " + player + " | " + "COMPUTER SCORE: " + computer + " | " + "TIE COUNTER: " + tieCounter;
+        }
+        
+
+        if (player === 5 || computer === 5) {
+            overlay.style.display = "block";
+            if (player > computer) {
+                gameOutcome.textContent = "YOU ARE WORTHY!";
+            } else {
+                gameOutcome.textContent = "TRY YOUR LUCK AGAIN !"
+            }
+    
+        }
+    
+    
+        restart.addEventListener('click', restartGame);
+        
+        
+      
+    });
+});
+
 
 function getComputerChoice()
 {
@@ -102,7 +159,31 @@ function playRound(playerSelection, computerSelection) {
     
   }
   
+function displayComputerchoice(computerSelection)
+{
+    if(computerSelection==="Rock")
+    compchoice.innerHTML = '<img width="100" height="100" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffreepngimg.com%2Fthumb%2Frock%2F8-2-rock-png-picture-thumb.png&f=1&nofb=1&ipt=9cb3db63b15372630baf99c45261121ba7a516df847035c4263e64dc66a03cb9&ipo=images">';
+    else if(computerSelection==="Paper")
+    compchoice.innerHTML = '<img width="100" height="100" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.textures4photoshop.com%2Ftex%2Fthumbs%2Ffree-burnt-paper-texture-background-thumb35.png&f=1&nofb=1&ipt=d0caba553c7aabd13c2a6e03e370352dbf1bfc77047afb530e203a0650808ab7&ipo=images">';
+    else if(computerSelection==="Scissors")
+    compchoice.innerHTML = '<img width="100" height="100" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn0.rubylane.com%2Fshops%2Fchelseaantiques%2FRL00962.1L.jpg%3F52&f=1&nofb=1&ipt=29d75104cbb2c1c6849d06f30e8818cb1b4fd47b165de459bb80533d87e30780&ipo=images">';
+    
+}
 
+function restartGame() {
+    overlay.style.display = "none";
+    computer = 0;
+    player = 0;
+    tieCounter=0;
+    content.textContent= " "; 
+
+    playerChoice.innerHTML = "";
+    computerChoice.innerHTML = "";
+    currentScore.textContent = `${playerScore} : ${computerScore}`;
+    roundOutcome.textContent = "First to score 5 points wins the game!";
+}
+
+/*older function*/
 function playGame()
 {
     var player=0;
@@ -142,76 +223,3 @@ function playGame()
     }
     
 }
-
-var player=0;
-var computer =0;
-var tieCounter=0;    
-const display=document.querySelector(".contain");
-const content=document.querySelector(".result");
-const compchoice=document.getElementById('compchoice');
-const gameOutcome = document.getElementById('gameOutcome');
-const restart = document.getElementById('restart');
-const overlay = document.getElementById('overlay');
-
-const btn=document.querySelectorAll("button");
-btn.forEach((button) => {
-
-    button.addEventListener('click',() => {
-
-        let computerSelection = getComputerChoice();
-
-        if(computerSelection==="Rock")
-        compchoice.innerHTML = '<img width="100" height="100" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffreepngimg.com%2Fthumb%2Frock%2F8-2-rock-png-picture-thumb.png&f=1&nofb=1&ipt=9cb3db63b15372630baf99c45261121ba7a516df847035c4263e64dc66a03cb9&ipo=images">';
-        else if(computerSelection==="Paper")
-        compchoice.innerHTML = '<img width="100" height="100" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.textures4photoshop.com%2Ftex%2Fthumbs%2Ffree-burnt-paper-texture-background-thumb35.png&f=1&nofb=1&ipt=d0caba553c7aabd13c2a6e03e370352dbf1bfc77047afb530e203a0650808ab7&ipo=images">';
-        else if(computerSelection==="Scissors")
-        compchoice.innerHTML = '<img width="100" height="100" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn0.rubylane.com%2Fshops%2Fchelseaantiques%2FRL00962.1L.jpg%3F52&f=1&nofb=1&ipt=29d75104cbb2c1c6849d06f30e8818cb1b4fd47b165de459bb80533d87e30780&ipo=images">';
-        
-
-        content.textContent= "ROUND RESULT: " + (playRound(button.id,computerSelection)); 
-        content.append(document.createElement("BR"));
-        const newP = document.createElement('p');
-        content.append(newP);
-        if(content.textContent === "ROUND RESULT: Won"){
-        player ++;
-        newP.textContent = "YOUR SCORE: " + player +  " | " + "COMPUTER SCORE: " + computer + " | " + "TIE COUNTER: " + tieCounter;
-
-        }
-        else if(content.textContent === "ROUND RESULT: Lost"){
-            computer ++;
-            newP.textContent = "YOUR SCORE: " + player + " | " + "COMPUTER SCORE: " + computer + " | " + "TIE COUNTER: " + tieCounter;
-        }
-        else if(content.textContent === "ROUND RESULT: Tie"){
-            tieCounter ++;
-            newP.textContent = "YOUR SCORE: " + player + " | " + "COMPUTER SCORE: " + computer + " | " + "TIE COUNTER: " + tieCounter;
-        }
-        
-        /*function isGameOver() {*/
-            if (player === 5 || computer === 5) {
-                overlay.style.display = "block";
-                if (player > computer) {
-                    gameOutcome.textContent = "YOU ARE WORTHY!";
-                } else {
-                    gameOutcome.textContent = "TRY YOUR LUCK AGAIN !"
-                }
-        
-            }
-        /*}*/
-        
-        restart.addEventListener('click', restartGame);
-        
-        function restartGame() {
-            overlay.style.display = "none";
-            computer = 0;
-            player = 0;
-            tieCounter=0;
-            content.textContent= " "; 
-
-            playerChoice.innerHTML = "";
-            computerChoice.innerHTML = "";
-            currentScore.textContent = `${playerScore} : ${computerScore}`;
-            roundOutcome.textContent = "First to score 5 points wins the game!";
-        }
-      
-    });
-});
